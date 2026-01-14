704a7d95cc4fde0b1550293a5b64292918242c5b
ZVanoZ <edu.ZVanoZ@gmail.com>
2026-01-14 17:39:19 +0200

    Перенес в ./docs/ файлы [DOCKER-OPTIONS.md; how-to-run-in-linux.md; run-app.sh]

--- docs/run-app.sh
+++ docs/run-app.sh
@@ -1,7 +1,9 @@
+# @DEPRECATED: используйте scripts в package.json, либо Docker
 #
 # sudo setcap 'cap_net_bind_service=+ep' ./run-app.sh
 # netstat -antpl | grep ":53\s"
 
 type node
 node -v
-node ./app/index.js
+cd ..
+node ./dist/app.js
